3695c47a91d406e89bf993a8acb270c7900525c0
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-10-05 23:07:53 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -76,7 +76,7 @@ const NextFactIntentHandler = {
     },
     handle(handlerInput) {
         if (facts.length === 1) {
-        const speakOutput = "Out of clue. You lost. The animal is a " + PickedAnimal + ". Did you know that it " + facts.pop() + "See you next time";
+        const speakOutput = "Out of clue. You lost. The animal is a " + PickedAnimal.toLowerCase() + ". Did you know that it " + facts.pop() + "See you next time";
         const repromptOutput = "sorry";
         return handlerInput.responseBuilder
             .speak(speakOutput)
