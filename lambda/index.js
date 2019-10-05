fc5b41da97e3e2a83c0063db5361a05b772436b3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-10-05 05:32:35 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -181,7 +181,6 @@ const Data = [{
 const PickedAnimalIndex = Math.floor(Math.random() * Data.length);
 const PickedAnimal = Data[PickedAnimalIndex].name;
 
-
 // The SkillBuilder acts as the entry point for your skill, routing all request and response
 // payloads to the handlers above. Make sure any new handlers or interceptors you've
 // defined are included below. The order matters - they're processed top to bottom.
